097daf2ea8c82bb961688ff09a6e3aab7919a8e5
Ushan Perera <gushanperera@gmail.com>
2025-03-01 23:42:36 +1030

    routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => {
+          return import('./home/home.component').then((m) => m.HomeComponent);
+        },
+      },
+      {
+        path: 'todos',
+        loadComponent: () => {
+          return import('./todos/todos.component').then((m) => m.TodosComponent);
+        },
+      },
+      {
+        path: 'examples',
+        loadComponent: () => {
+          return import('./component/example/varable/varable.component').then((m) => m.VarableComponent);
+        },
+      },
+
+];
