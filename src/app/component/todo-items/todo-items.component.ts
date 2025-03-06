ce593d09f506ec1bbb739b5b2b8dfcf1544aab73
Ushan Perera <gushanperera@gmail.com>
2025-03-06 11:08:24 +1030

    5

---
+++ src/app/component/todo-items/todo-items.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-items',
+  imports: [],
+  templateUrl: './todo-items.component.html',
+  styleUrl: './todo-items.component.scss'
+})
+export class TodoItemsComponent {
+
+}
